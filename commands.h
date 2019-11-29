434374d43880b09048cf6518af4d5d25eb268a15
root <root@localhost.localdomain>
2019-11-29 14:34:58 +0300

    Saving everything

--- commands.h
+++ commands.h
@@ -1,8 +1,8 @@
 //DEF_DIF(name, mode, wchar_t equivalent, code, do)
 
-#define dL do_diff_recursive(node->left)
+#define dL do_diff_recursive(node->left, cur_diff)
 #define cL node_copy(node->left)
-#define dR do_diff_recursive(node->right)
+#define dR do_diff_recursive(node->right, cur_diff)
 #define cR node_copy(node->right)
 #define NEW(mode, elem, left, right) node_create_new(mode, elem, left, right)
 #define L LEFT->data
@@ -10,7 +10,7 @@
 
 DEF_DIF(cst, MODE_CNST, '\0' , return NEW(MODE_CNST, 0, nullptr, nullptr);, 0;)
 
-DEF_DIF(var, MODE_VARL, '\0' , return NEW(MODE_CNST, 1, nullptr, nullptr);, 0;)
+DEF_DIF(var, MODE_VARL, '\0' , return (DATA == cur_diff || cur_diff == -1) ? NEW(MODE_CNST, 1, nullptr, nullptr) : NEW(MODE_CNST, 0, nullptr, nullptr);, 0;)
 
 DEF_DIF(add, MODE_FUNC, '+', return NEW(MODE_FUNC, '+', dL, dR);, DATA = L + R)
 
